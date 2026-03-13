ab3d2489be285b3768012bc05209aed049993f15
Klerno-Labs <228250850+Klerno-Labs@users.noreply.github.com>
2026-03-13 16:09:49 -0500

    fix: add "use client" to 1 component using client-only features

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { siteConfig } from "@/config/site";
 import { Facebook, Instagram, Linkedin, Twitter, ArrowUp, MapPin, Mail, Phone } from "lucide-react";
